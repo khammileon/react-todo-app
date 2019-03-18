f537a6f035d7c1bddf1d3cfde1ccea7b4981ae26
Sara Cammi <cammisara@outlook.fr>
2019-03-18 17:31:03 +0100

    First commit

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Apprendre React",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Créer une chaîne YouTube",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Sortir les chiennes",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Faire du sport",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Nettoyer l'Opel Corsa",
+        completed: false
+    }
+]
+
+export default todosData
